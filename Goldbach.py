ef2801d0dae09420717f67dbd008117ae7c50784
Craig W Fouts <41246125+cwpfautz@users.noreply.github.com>
2019-11-28 09:01:23 -0500

    Update Goldbach.py

--- Goldbach.py
+++ Goldbach.py
@@ -1,31 +1,22 @@
-import math as m
+import util
+import Wilson
 
-def isPrime(num): 
-    return m.factorial(num-1)%num == (-1)%num
 
-def rightNum(num):
-    if num%2 != 0 or num <=2:
-        return False
-    else:
-        return True
+def find_sums(n):
+    """Find and return a list of sets of primes that sum to n"""
 
-def runNum(num): 
-    primes = []
-    i = 1
-    while i < num:
-        if isPrime(i) and isPrime(num - i):
-            if primes.__contains__([num - i, i]):
-                i += 1
-            else:
-                primes.append([i, num - i])
-            i += 1
-        else:
-            i += 1
-    return primes
+    sums = []  # Sets of primes that sum to n
 
-if __name__=="__main__":
-    num = int(input("Enter an even integer greater than two: "))
-    if rightNum(num):
-        print("{0} can be expressed as the sum of these {1} pairs of prime numbers:\n{2}".format(num,len(runNum(num)),runNum(num)))
-    else:
-        print("Is {0} an even integer greater than two? No. No it isn't.".format(num))
+    for i in range(1, n):
+        if Wilson.is_prime(i) and Wilson.is_prime(n - i):
+            if [n - i, i] not in sums:
+                sums.append([i, n - i])
+
+    return sums
+
+
+if __name__ == '__main__':
+    num = util.get_num(lim=2)
+    print("{0} can be expressed as:".format(num))
+    for sum_ in find_sums(num):
+        print("{0} + {1}".format(sum_[0], sum_[1]))
